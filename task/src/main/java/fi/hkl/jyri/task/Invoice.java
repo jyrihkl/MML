56f9606586929bab93dc7d169f6cece2b52e08e0
Jyri Hakala <jyri.hakala@tuni.fi>
2023-05-01 19:09:44 +0300

    Add getters & setter for Invoice

--- task/src/main/java/fi/hkl/jyri/task/Invoice.java
+++ task/src/main/java/fi/hkl/jyri/task/Invoice.java
@@ -9,6 +9,38 @@ public class Invoice {
     private Date dueOn;
     private Date paidOn;
 
+    public String getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(String invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public Date getBilledOn() {
+        return billedOn;
+    }
+
+    public void setBilledOn(Date billedOn) {
+        this.billedOn = billedOn;
+    }
+
+    public Date getDueOn() {
+        return dueOn;
+    }
+
+    public void setDueOn(Date dueOn) {
+        this.dueOn = dueOn;
+    }
+
+    public Date getPaidOn() {
+        return paidOn;
+    }
+
+    public void setPaidOn(Date paidOn) {
+        this.paidOn = paidOn;
+    }
+
     public Invoice(String invoiceId, Date billedOn, Date dueOn, Date paidOn) {
         this.invoiceId = invoiceId;
         this.billedOn = billedOn;
