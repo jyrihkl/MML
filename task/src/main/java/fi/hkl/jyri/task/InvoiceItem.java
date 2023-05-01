12208e0df7b9a6628c2831b2d5bd1e407db29a25
Jyri Hakala <jyri.hakala@tuni.fi>
2023-05-01 17:49:42 +0300

    Implement InvoiceItem

---
+++ task/src/main/java/fi/hkl/jyri/task/InvoiceItem.java
@@ -0,0 +1,40 @@
+package fi.hkl.jyri.task;
+
+import java.util.Date;
+
+public class InvoiceItem {
+
+    private Product product;
+    private Date date;
+    private Integer amount;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public InvoiceItem(Product product, Date date, Integer amount) {
+        this.product = product;
+        this.date = date;
+        this.amount = amount;
+    }
+}
